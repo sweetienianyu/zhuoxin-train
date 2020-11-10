5a83e00109f47e425ed63ba65bea53b13eb9b86e
sweetienianyu <475493955@qq.com>
2020-11-10 13:11:48 +0800

    todo-list 条件筛选

--- 02-todo/todo-list/src/components/todo/index.vue
+++ 02-todo/todo-list/src/components/todo/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="todo-wrap">
-    <el-input class="todo-input" v-model.trim="todo_text" @keyup.enter.native="handleEnter" placeholder="What needs to be done?" ></el-input>
+    <el-input class="todo-input" v-model.trim="todo_text" @keyup.enter.native="handleEnter" placeholder="What needs to be done?" >
+       <i slot="prefix" class="el-input__icon el-icon-arrow-down" @click.stop="handleAllChecked"></i>
+    </el-input>
     <div class="todo-content">
-      <item v-for="(item, index) in todo_list" :key="index" :text="item.text" :checked="item.checked" @yudan="handleYudan(index, $event)" @remove="handleRemove(index)"/>
+      <item v-for="(item, index) in activeList" :key="index" :text="item.text" :checked="item.checked" @yudan="handleYudan(index, $event)" @remove="handleRemove(index)"/>
+      <div class="todo—bottom">
+        <div>{{ count }} items left</div>
+        <div class="todu-screen">
+          <div class="screenStyle">all</div>
+          <div class="screenStyle" @click="handleFilterActive">Active</div>
+          <div class="screenStyle">Completed</div>
+        </div>
+        <div class="clear">Clear completed</div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,11 +24,40 @@ export default {
   data() {
     return {
       todo_text: '',
-      todo_list: []
+      todo_list: [],
+      isChecked: true,
+      isScreen: false,
+      status: '', // 当前状态
     }
   },
   components: {
     item
+  },
+  computed: {
+    count() {
+      // var result = []
+      //   for(var i=0;i<this.todo_list.length;i++){
+      //      result = this.todo_list[i].filter(this.todo_list[i].checked !== true)
+      //       console.log(result)
+      //   }
+      // return result.length
+      const result = this.todo_list.filter(function(item) {
+        return item.checked === false
+      })
+      return result.length
+    },
+    activeList() {
+      if(this.status === '') {
+        return this.todo_list
+      }
+      if(this.status === 'active') {
+        const result = this.todo_list.filter(function(item) {
+          return item.checked === false
+        })
+        return result
+      }
+      return this.todo_list
+    }
   },
   methods: {
     handleEnter() {
@@ -37,12 +77,31 @@ export default {
     },
     handleYudan(index, val) {
       this.todo_list[index].checked = val
+      
     },
     handleRemove(index){
       this.todo_list.splice(this.todo_list[index],1)
     },
+    // 全选
+    handleAllChecked() {
+      if(this.isChecked) {
+        // 选中
+        for(let i =0 ;i<this.todo_list.length;i++){
+            this.todo_list[i].checked = true
+           }
+           this.isChecked = false
+      }else {
+        for(let i =0 ;i<this.todo_list.length;i++){
+            this.todo_list[i].checked = false
+           }
+           this.isChecked = true
+      }
+    },
+    // active状态筛选
+    handleFilterActive() {
+      this.status = 'active'
+    },
     // TODO
-    // 1 全选 反选
     // 2 不同状态的筛选
     // 3 编辑
     // 4 持久化
@@ -65,5 +124,26 @@ export default {
   border: 1px solid #eaeaea;
   
 }
+.todo—bottom{
+  display: flex;
+  padding: 10px 20px;
+  justify-content: space-between;
+}
+.todu-screen{
+  display: flex;
+
+}
+.screenStyle{
+  padding: 5px;
+}
+.screenStyle:hover{
+  /* padding: 0 5px; */
+  outline: 1px solid orange;
+  border-radius: 5px;
+
+  }
+ .clear:hover{
+  text-decoration:underline;
+  }
 </style>
 
